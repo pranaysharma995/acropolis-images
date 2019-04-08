9e866aad6e52c6994f6050d5b085849f104c57ea
shristiverma <shristiverma2088@gmail.com>
2019-04-08 10:30:23 +0530

    healthcare background

--- gulpfile.js
+++ gulpfile.js
@@ -10,16 +10,16 @@ gulp.task("jpeg",function()
 })
 gulp.task("jpeg",function()
         {
-   return gulp.src("banner/*.jpeg").pipe(imagemin(
+   return gulp.src("industries/healthacare/*.jpg").pipe(imagemin(
    {
        progressive:true
    })).pipe(imagemin([imageminMozjpeg({
        quality:80
-   })])).pipe(gulp.dest("compress/banner/"));
+   })])).pipe(gulp.dest("compress/healthcare"));
 });
 gulp.task("shristi",function()
          {
-    return gulp.src("banner/*.png").pipe(imagemin([imageminGuetzli({
+    return gulp.src("industries/healthcare/*.png").pipe(imagemin([imageminGuetzli({
         quality:85
-    })])).pipe(gulp.dest("compress/banner/"));
+    })])).pipe(gulp.dest("compress/healthcare"));
 })
